be7a5a4b88fc6204d1151501d5be11df42d7ee66
vineethpradeep <vineethpadeeps@gmail.com>
2024-08-13 00:26:35 +0100

    alignment correction

--- app/contactus/page.jsx
+++ app/contactus/page.jsx
@@ -26,7 +26,7 @@ const ContactusPage = () => {
             ></polygon>
           </svg>
         </div>
-        <div className="container mx-auto px-4 pt-8">
+        <div className="container mx-auto px-4 lg:pb-20 pt-8">
           <div className="flex flex-wrap text-center justify-center">
             <div className="w-full lg:w-6/12 px-4">
               <h2 className="text-4xl font-semibold text-white">
@@ -79,7 +79,7 @@ const ContactusPage = () => {
           </div>
         </div>
       </section>
-      <section className="relative block py-24 lg:pt-0 bg-gray-900">
+      <section className="relative block py-24 lg:pt-24 bg-gray-900">
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap justify-center lg:-mt-64 -mt-48">
             <div className="w-full lg:w-6/12 px-4">
