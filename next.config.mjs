fba886dab7f9a90c3a3cec57c147eb34ec27377f
vineethpradeep <vineethpadeeps@gmail.com>
2024-08-13 01:18:58 +0100

    removed proxy

--- next.config.mjs
+++ next.config.mjs
@@ -16,14 +16,6 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:3000/api/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
