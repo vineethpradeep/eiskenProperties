6f2a24c83c4fbc2d7666730b33d459689a6068d3
vineethpradeep <vineethpadeeps@gmail.com>
2024-08-13 01:01:23 +0100

    add proxy fix

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
